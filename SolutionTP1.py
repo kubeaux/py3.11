ae7b2da7dc9cf2df3545ed4b4a1cd45d70225fb4
natelfy <kubeaux@gmail.com>
2026-02-02 15:37:38 +0100

    TD1 finit

--- SolutionTP1.py
+++ SolutionTP1.py
@@ -48,3 +48,49 @@ def see_lines(width, height, lines):
                 res[i, j_start] = 1
     return res
             
+def see_quadrant_complete(image_name, quadrant):
+    img = mpimg.imread(image_name)
+    
+    if img.dtype == np.uint8:
+        img = img.astype(float) / 255.0
+    else:
+        img = img.copy()
+
+    if len(img.shape) == 2:
+        img = np.stack((img,)*3, axis=-1)
+    
+    if img.shape[2] > 3:
+        img = img[:, :, :3]
+        
+    h, w = img.shape[:2]
+    
+    img[h // 2, :, :] = [1.0, 0.0, 0.0]
+    
+    img[:, w // 2, :] = [1.0, 0.0, 0.0]
+    
+    t_i = 0
+    t_j = 0
+    
+    if quadrant == 'top_left':
+        t_i = h // 4
+        t_j = w // 4
+    elif quadrant == 'top_right':
+        t_i = h // 4
+        t_j = (3 * w) // 4
+    elif quadrant == 'bottom_left':
+        t_i = (3 * h) // 4
+        t_j = w // 4
+    elif quadrant == 'bottom_right':
+        t_i = (3 * h) // 4
+        t_j = (3 * w) // 4
+        
+    delta = 3
+    
+    r_start = max(0, t_i - delta)
+    r_end = min(h, t_i + delta + 1)
+    c_start = max(0, t_j - delta)
+    c_end = min(w, t_j + delta + 1)
+    
+    img[r_start:r_end, c_start:c_end, :] = [0.0, 0.0, 1.0]
+    
+    return img
