cc83f18d5dd5c7e77e1ff6f985ed206483dd3a0f
natelfy <kubeaux@gmail.com>
2026-02-07 16:40:19 +0100

    faux

--- SolutionTP2.py
+++ SolutionTP2.py
@@ -1,22 +1,20 @@
+import numpy as np
+
 def see_lines(width, height, lines):
     res = np.zeros((height, width), dtype=int)
-
     for (i_start, j_start, i_end, j_end) in lines:
-        x1 = j_start
-        y1 = i_start
-        x2 = j_end
-        y2 = i_end
-
-        e = x2 - x1
-        dx = e * 2
-        dy = (y2 - y1) * 2
-
+        x1, y1 = i_start, j_start
+        x2, y2 = i_end, j_end
+        
+        dx = x2 - x1
+        dy = y2 - y1
+        e = 2 * dy - dx
+        
         while x1 <= x2:
-            res[y1, x1] = 1
-            x1 = x1 + 1
-            e = e - dy
-            if e <= 0:
+            res[x1, y1] = 1
+            if e >= 0:
                 y1 = y1 + 1
-                e = e + dx
-
+                e = e - 2 * dx
+            x1 = x1 + 1
+            e = e + 2 * dy
     return res
