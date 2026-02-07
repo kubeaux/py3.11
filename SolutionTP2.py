6b0fb60beca4de6a22dc9c7b62695912985ec3d6
natelfy <kubeaux@gmail.com>
2026-02-07 23:32:23 +0100

    réussi enfin

--- SolutionTP2.py
+++ SolutionTP2.py
@@ -1,49 +1,45 @@
 import numpy as np
 
 def see_lines(height, width, lines):
-    # Création de l'image noire (matrice de 0)
     res = np.zeros((height, width), dtype=int)
     
     for (i_start, j_start, i_end, j_end) in lines:
-        # Correspondance des coordonnées :
-        # Dans l'algo Wikipedia : x est la colonne, y est la ligne (rangée)
-        # Dans la matrice numpy : j est la colonne, i est la ligne
-        
         x1, y1 = j_start, i_start
         x2, y2 = j_end, i_end
         
-        # --- DÉBUT ALGORITHME BRESENHAM (Wikipedia) ---
+        is_steep = abs(y2 - y1) > abs(x2 - x1)
+        if is_steep:
+            x1, y1 = y1, x1
+            x2, y2 = y2, x2
+            
+        if x1 > x2:
+            x1, x2 = x2, x1
+            y1, y2 = y2, y1
+            
+        ystep = 1 if y1 < y2 else -1
         
-        # e ← x2 - x1
         e = x2 - x1
         
-        # dx ← e × 2
-        dx = e * 2
+        dx = e * 2  
         
-        # dy ← (y2 - y1) × 2
-        dy = (y2 - y1) * 2
+        dy = abs(y2 - y1) * 2 
         
-        # tant que x1 ≤ x2 faire
-        while x1 <= x2:
-            # tracerPixel(x1, y1)
-            # (On vérifie juste qu'on ne sort pas de l'image)
-            if 0 <= x1 < width and 0 <= y1 < height:
-                res[y1, x1] = 1
+        x_curr = x1
+        y_curr = y1
+        
+        while x_curr <= x2:
+            if is_steep:
+                if 0 <= x_curr < height and 0 <= y_curr < width:
+                    res[x_curr, y_curr] = 1
+            else:
+                if 0 <= y_curr < height and 0 <= x_curr < width:
+                    res[y_curr, x_curr] = 1
             
-            # x1 ← x1 + 1  // colonne du pixel suivant
-            x1 = x1 + 1
+            x_curr = x_curr + 1
             
-            # si (e ← e - dy) ≤ 0 alors
             e = e - dy
             if e <= 0:
-                # y1 ← y1 + 1  // choisir plutôt le pixel suivant dans la rangée supérieure
-                y1 = y1 + 1
-                
-                # e ← e + dx  // ajuste l’erreur
+                y_curr = y_curr + ystep
                 e = e + dx
-            # fin si
-        # fin faire
-        
-        # --- FIN ALGORITHME ---
         
     return res
