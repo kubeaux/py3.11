ff41cd0c3abf805efb54df7ffc86fcdde8df683e
natelfy <kubeaux@gmail.com>
2026-02-07 23:22:09 +0100

    nul

--- SolutionTP2.py
+++ SolutionTP2.py
@@ -1,47 +1,24 @@
 import numpy as np
 
-def see_lines(width, height, lines):
-    """
-    Adaptation du see_lines du TP1 avec l'algorithme de Bresenham du TP2.
-    Signature conservée : (width, height, lines).
-    """
-    # 1. Structure issue du TP1 (Validée)
-    res = np.zeros((height, width), dtype=int)
+def see_lines(height, width, lines):
+    image = np.zeros((height, width), dtype=int)
+
+    for line in lines:
+        i_start, j_start, i_end, j_end = line
+        
+        x1, y1 = j_start, i_start
+        x2, y2 = j_end, i_end
     
-    for (i_start, j_start, i_end, j_end) in lines:
-        # 2. Mapping des coordonnées
-        # TP1 utilise (i, j) -> (ligne, colonne)
-        # Bresenham (1er octant) utilise x principal -> x correspond aux colonnes (j)
-        x1 = int(j_start)
-        y1 = int(i_start)
-        x2 = int(j_end)
-        y2 = int(i_end)
-        
-        # 3. Normalisation "Arbitrairement orientées"
-        # L'algorithme 'tant que x1 <= x2' impose que x1 soit le point de gauche.
-        # Si la ligne est définie de droite à gauche, on inverse les points.
-        if x1 > x2:
-            x1, x2 = x2, x1
-            y1, y2 = y2, y1
-            
-        # 4. Algorithme de Bresenham (Consigne TP2 / Wikipedia)
-        e = x2 - x1          # Erreur initiale
-        dx = e * 2           # Constante dx
-        dy = (y2 - y1) * 2   # Constante dy
-        
+        e = x2 - x1
+        dx = e * 2
+        dy = (y2 - y1) * 2
         while x1 <= x2:
-            # Tracé du pixel : res[ligne, colonne] => res[y, x]
-            if 0 <= y1 < height and 0 <= x1 < width:
-                res[y1, x1] = 1
-            
-            # Avance sur l'axe colonne (x)
+            if 0 <= x1 < width and 0 <= y1 < height:
+                image[y1, x1] = 1
             x1 = x1 + 1
-            
-            # Mise à jour de l'erreur
-            # "si (e <- e - dy) <= 0"
             e = e - dy
             if e <= 0:
-                y1 = y1 + 1  # Montée sur l'axe ligne (y)
-                e = e + dx   # Ajustement erreur
-                
-    return res
+                y1 = y1 + 1
+                e = e + dx
+
+    return image
